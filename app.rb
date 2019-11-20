4be5e6f921761e2653968f6280e877daaddc2a17
Lucy Matejka <lucymatejka@yahoo.co.uk>
2019-11-20 15:52:07 +0000

    POST redirect route added

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'sinatra/base'
 require 'sinatra'
+enable :sessions
 
 class Battle < Sinatra::Base
 
@@ -8,13 +9,15 @@ class Battle < Sinatra::Base
  end
 
   post '/names' do
+    session[:player_1_name] = params[:player_1_name]
+    session[:player_2_name]= params[:player_2_name]
     erb :index
   end
 
   get '/play' do
-   @player_1_name = params[:player_1_name]
-   @player_2_name = params[:player_2_name]
-   erb :play
+   @player_1_name = session[:player_1_name]
+   @player_2_name = session[:player_2_name]
+   redirect '/play'
   end
 
   run! if app_file == $0
