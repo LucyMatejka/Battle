b7e6dedb9d686a1b23c112839185f77ff70ccacc
Lucy Matejka <lucymatejka@yahoo.co.uk>
2019-11-21 12:02:57 +0000

    add web_helpers

--- app.rb
+++ app.rb
@@ -11,13 +11,13 @@ class Battle < Sinatra::Base
   post '/names' do
     session[:player_1_name] = params[:player_1_name]
     session[:player_2_name]= params[:player_2_name]
-    erb :index
+    redirect '/play'
   end
 
   get '/play' do
    @player_1_name = session[:player_1_name]
    @player_2_name = session[:player_2_name]
-   redirect '/play'
+   erb :play
   end
 
   run! if app_file == $0
--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,9 +1,6 @@
 feature 'Enter names' do
   scenario 'submitting names' do
-    visit('/')
-    fill_in :player_1_name, with: 'Lucy'
-    fill_in :player_2_name, with: 'Mislav'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Player 1 is Lucy'
     expect(page).to have_content 'Player 2 is Mislav'
   end
